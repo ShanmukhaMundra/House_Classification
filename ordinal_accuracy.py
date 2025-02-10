8726bb2953da99a2bd9f49f6f566cc74b5d83662
shanu0006 <shanumundra66@gmail.com>
2025-02-10 11:40:07 -0500

    ordinal encoding file

---
+++ ordinal_accuracy.py
@@ -0,0 +1,31 @@
+import pandas as pd
+df = pd.read_csv('/Users/shanmukhamundra/Desktop/House_Clsfcation/house_class.csv')
+from sklearn.model_selection import train_test_split
+X = df[['Area', 'Room', 'Lon', 'Lat', 'Zip_area', 'Zip_loc']]
+y = df['Price']
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, stratify=X['Zip_loc'].values, random_state=1
+)
+from sklearn.preprocessing import OrdinalEncoder
+encoder = OrdinalEncoder()
+encoder.fit(X_train[['Zip_area', 'Zip_loc', 'Room']])
+encoded_train = encoder.transform(X_train[['Zip_area', 'Zip_loc', 'Room']])
+encoded_test = encoder.transform(X_test[['Zip_area', 'Zip_loc', 'Room']])
+encoded_train_df = pd.DataFrame(encoded_train, columns=encoder.get_feature_names_out())
+encoded_test_df = pd.DataFrame(encoded_test, columns=encoder.get_feature_names_out())
+X_train_final = X_train[['Area', 'Lon', 'Lat']].join(encoded_train_df)
+X_test_final = X_test[['Area', 'Lon', 'Lat']].join(encoded_test_df)
+from sklearn.tree import DecisionTreeClassifier
+model = DecisionTreeClassifier(
+    criterion='entropy',
+    max_features=3,
+    splitter='best',
+    max_depth=6,
+    min_samples_split=4,
+    random_state=3
+)
+model.fit(X_train_final, y_train)
+predictions = model.predict(X_test_final)
+from sklearn.metrics import accuracy_score
+accuracy_ordinal = accuracy_score(y_test, predictions)
+print(accuracy_ordinal)
